9fe5b8bea4519d68afef2b3904f517890c28a103
PGVlasov <PGVlasov@users.noreply.github.com>
2019-04-29 23:30:14 +0300

    homework is done

---
+++ document-structure/cart/task.js
@@ -0,0 +1,46 @@
+const prodCart = document.querySelector('.cart__products');
+const reduceBtn = document.querySelectorAll('.product__quantity-control_dec');
+const increaseBtn = document.querySelectorAll('.product__quantity-control_inc');
+const prodAdd = document.querySelectorAll('.product__add');
+
+for(let btn of reduceBtn) {
+	btn.addEventListener('click', (event) => {
+		const parent = btn.parentNode;
+		const val = parent.childNodes[3];
+		let value = val.textContent;
+		if(value > 1) val.textContent = --value;
+	});
+}
+
+for(let btn of increaseBtn) {
+	btn.addEventListener('click', (event) => {
+		const parent = btn.parentNode;
+		const val = parent.childNodes[3];
+		let value = val.textContent;
+		val.textContent = ++value;
+	});
+}
+
+for(let btn of prodAdd) {
+	btn.addEventListener('click', (event) => {
+		const parent = event.target.closest('.product');
+		const id = parent.dataset.id;
+		const src = parent.childNodes[3].src;
+		const value = parent.children[2].children[0].children[1].children[1].textContent;
+		html(id, src, value);
+	});
+}
+
+function html(id, src, counter) {
+	const children = Array.from(prodCart.children);
+	const block = children.find((block) => block.dataset.id === id);
+	if(block) {
+		const value = parseInt(block.children[1].textContent);
+		block.children[1].textContent = value + parseInt(counter);
+		return 0;
+	}
+	prodCart.innerHTML += `<div class="cart__product" data-id="${id}">
+				                <img class="cart__product-image" src="${src}">
+				                <div class="cart__product-count">${counter}</div>
+				            </div>`;
+}
---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,24 @@
+let texts = document.querySelectorAll("has-tooltip")
+let toolTip = document.queryCommandValue('.tooltip')
+let tooltipActive = document.querySelector('.tooltip_active');
+
+for (let text of texts)  {
+    text.addEventListener('click', showAdvice);
+}
+
+toolTip.addEventListener('click', () => toolTip.classList.remove('tooltip_active'));
+
+function showAdvice(e) {
+
+    e.preventDefault();
+
+    if (tooltipActive) {
+        tooltipActive.classList.remove('tooltip_active');
+    }
+
+    toolTip.classList.add('tooltip_active');
+    toolTip.innerText = event.target.title;
+    toolTip.style.top = event.target.getBoundingClientRect().top + 20 + 'px';
+    toolTip.style.left = event.target.getBoundingClientRect().left + 'px';
+}
+
