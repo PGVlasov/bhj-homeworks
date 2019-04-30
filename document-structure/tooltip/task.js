02ac3644446507cdcfa11a70a81a413cb2dfb669
PGVlasov <PGVlasov@users.noreply.github.com>
2019-05-01 00:19:22 +0300

    Заработало!

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,14 @@
-let texts = document.querySelectorAll("has-tooltip")
-let toolTip = document.querySelector('.tooltip')
-let tooltipActive = document.querySelector('.tooltip_active');
+const texts = document.querySelectorAll('.has-tooltip');
+const toolTip = document.querySelector('.tooltip');
+const tooltipActive = document.querySelector('.tooltip_active');
 
-for (let text of texts)  {
-    text.addEventListener('click', showAdvice);
+for (let txt of texts) {
+    txt.addEventListener('click', showAdvice);
 }
 
 toolTip.addEventListener('click', () => toolTip.classList.remove('tooltip_active'));
 
 function showAdvice(e) {
-
     e.preventDefault();
 
     if (tooltipActive) {
@@ -17,8 +16,7 @@ function showAdvice(e) {
     }
 
     toolTip.classList.add('tooltip_active');
-    toolTip.innerText = event.target.title;
-    toolTip.style.top = event.target.getBoundingClientRect().top + 20 + 'px';
-    toolTip.style.left = event.target.getBoundingClientRect().left + 'px';
+    toolTip.innerText = e.target.title;
+    toolTip.style.top = e.target.getBoundingClientRect().top + 20 + 'px';
+    toolTip.style.left = e.target.getBoundingClientRect().left + 'px';
 }
-
